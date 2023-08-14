df44683ebf46fc4f3072dd9f94310f42e6192d70
Janni <barstadjannimaria@gmail.com>
2023-08-14 17:44:07 +0200

    jmb
    ormdata

---
+++ ORDDATA/ORMData/ORM/Models/Grade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ORM.Models;
+
+public partial class Grade
+{
+    public int GradeId { get; set; }
+
+    public int? GradeValue { get; set; }
+
+    public int? StudentId { get; set; }
+
+    public int? ProfessorId { get; set; }
+
+    public virtual Professor? Professor { get; set; }
+
+    public virtual Student? Student { get; set; }
+}
